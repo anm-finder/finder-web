3f295ede3a37d30bb6395641e80c3ebc75392147
Gleb Kalinin <sogener.g@gmail.com>
2022-06-16 21:29:51 +0500

    WIP: Add route view

--- main.go
+++ main.go
@@ -4,16 +4,35 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	urlPath := r.URL.Path[1:]
+type Page struct {
+	Title string
+	Body  []byte
+}
 
-	fmt.Fprintf(w, "Hi there, I love %s!", urlPath)
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
